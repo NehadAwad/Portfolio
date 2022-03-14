e98812ccc065b737cb187e5f40ce4e99e5f79f8e
NehadAwad <nehadakash622@gmail.com>
2022-03-14 23:18:55 +0600

    frontend assets added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.pages.dashboard.index');
 });
+
+Route::get('/a', function (){
+    return view('frontend.main');
+});
